cfba8ebafeb313ee2c3887f2b7869622df4fd022
Jonas Taeger <jonastaeger@MBP-von-Jonas.fritz.box>
2024-01-22 18:04:55 +0100

    Jellyseerr Support

---
+++ jellyseerr/rootfs/etc/cont-init.d/90-run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+# Create files
+CONFIG_LOCATION="/config/addons_config/jellyseer"
+bashio::log.info "Config stored in $CONFIG_LOCATION"
+mkdir -p "$CONFIG_LOCATION"
+cp -rnT /app/config "$CONFIG_LOCATION"/
+rm -r /app/config
+ln -s "$CONFIG_LOCATION" /app/config
+chmod -R 755 "$CONFIG_LOCATION"
+
+# Create files
+JELLYFIN_TYPE=$(bashio::config 'TYPE')
+export JELLYFIN_TYPE
+TZ=$(bashio::config 'TZ')
+export TZ
+
+yarn start
